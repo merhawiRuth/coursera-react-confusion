0730b839d2033cde5626ea06f93dae6c3f7728d2
merhawi <merhawi.temnewo88@gmail.com>
2020-02-15 14:57:45 -0600

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
            </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
