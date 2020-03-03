cdaa04759d9fddeb8cce2419095e9fdf740216b6
merhawi <merhawi.temnewo88@gmail.com>
2020-03-02 22:24:48 -0600

    Intro. to Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import { initialState, Reducer } from './reducer'
+
+
+
+export const configureStore = () => {
+    const store = createStore(
+        Reducer,
+        initialState,
+    )
+    return store;
+}
