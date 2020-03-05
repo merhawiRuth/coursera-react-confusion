2d3851bf0e5a8c73a54bd8d4897d3b9d0d93f400
merhawi <merhawi.temnewo88@gmail.com>
2020-03-04 23:28:41 -0600

    React Redux Form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,27 +1,137 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem
+    CardTitle, Breadcrumb, BreadcrumbItem, Button, Row, Col, Label, Modal, ModalBody, ModalHeader
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+
+    state = {
+        isModalOpen: false
+    };
+
+    toggleModal = () => {
+        this.setState({ isModalOpen: !this.state.isModalOpen })
+    }
+
+    render() {
+        return (
+            <div>
+                <a onClick={this.toggleModal} role="button" className="btn btn-outline-dark mt-1 "><i class="fa fa-pencil" aria-hidden="true"></i> Submit Comment</a>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="firstname" md={3}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" id="rating" name="rating"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </Control.select>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="firstname" md={3}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".yourname" id="yourname" name="yourname"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(2), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".yourname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="firstname" md={3}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea rows="6" model=".comment" id="comment" name="comment"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Control.button
+                                    model="user" className="btn btn-primary ml-3"
+                                >
+                                    Submit!
+                                </Control.button>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>)
+    }
+}
 
 const RenderComments = ({ comments }) => {
-
+    const comment = comments.map(comment => {
+        return (
+            <div key={comment.author}>
+                <CardBody >
+                    <li className="list-unstyled">
+                        <CardText>{comment.comment}</CardText>
+                        <CardText>{`--${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}`}</CardText>
+                    </li>
+                </CardBody>
+            </div>
+        )
+    })
     if (comments) {
-        const comment = comments.map(comment => {
-            return (
-                <div key={comment.author}>
-                    <CardBody >
-                        <li className="list-unstyled">
-                            <CardText>{comment.comment}</CardText>
-                            <CardText>{`--${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}`}</CardText>
-                        </li>
-                    </CardBody>
-                </div>
-            )
-        })
-        return comment;
+        return (<div>
+            {comment}
+            <CommentForm />
+        </div>);
     }
     else {
         return <div></div>
