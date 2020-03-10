195f25016f948f60021e0136efea31038ba690b9
merhawi <merhawi.temnewo88@gmail.com>
2020-03-10 17:20:38 -0500

    Redux Thunk

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,8 +2,6 @@ import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action) => {
-    console.log("inside comments reducer");
-
     switch (action.type) {
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
